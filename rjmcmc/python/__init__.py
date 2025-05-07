9b335b7299f96f18e91dfb4845fac3612ae87093
Malcolm Sambridge <44758198+msambridge@users.noreply.github.com>
2025-05-08 06:17:53 +1000

    updated using JH build

---
+++ rjmcmc/python/__init__.py
@@ -0,0 +1,27 @@
+from .rjmcmc import (
+    rjmcmc_seed,
+    dataset1d,
+    resultset1d,
+    resultset1dfm,
+    regression_single1d,
+    regression_single1d_sampled,
+    regression_part1d_zero,
+    regression_part1d_natural,
+    regression_part1d,
+    regression_part1d_sampled,
+    forwardmodel_part1d,
+)
+
+__all__ = [
+    'rjmcmc_seed',
+    'dataset1d',
+    'resultset1d',
+    'resultset1dfm',
+    'regression_single1d',
+    'regression_single1d_sampled',
+    'regression_part1d_zero',
+    'regression_part1d_natural',
+    'regression_part1d',
+    'regression_part1d_sampled',
+    'forwardmodel_part1d',
+]
